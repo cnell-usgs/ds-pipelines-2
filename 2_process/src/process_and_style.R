7555a7d90aa1ba41673819236b9b0a2265b81b36
Nell <cnell@usgs.gov>
2020-05-19 11:47:17 -0400

    exchange oject and file targets in pipeline

--- 2_process/src/process_and_style.R
+++ 2_process/src/process_and_style.R
@@ -1,23 +1,24 @@
-combine_nwis_data <- function(download_files){
 
+combine_nwis_data <- function(download_files = c('1_fetch/out/nwis_01427207_data.csv','1_fetch/out/nwis_01432160_data.csv','1_fetch/out/nwis_01435000_data.csv','1_fetch/out/nwis_01436690_data.csv','1_fetch/out/nwis_01466500_data.csv','1_fetch/out/nwis_01477050_data.csv')){
+  
   data_out <- data.frame(agency_cd = c(), site_no = c(), dateTime = c(), 
                          X_00010_00000 = c(), X_00010_00000_cd = c(), tz_cd = c())
   
   for (download_file in download_files){
-
+    
     # read the downloaded data and append it to the existing data.frame
     these_data <- read_csv(download_file, col_types = 'ccTdcc')
     data_out <- rbind(data_out, these_data)
   }
   
-  write_csv(data_out, '2_process/out/nwis_data.csv')
+  return(data_out)
   
 }
 
 
-process_and_style <- function(nwis_data_path = '2_process/out/nwis_data.csv', site_filename = '1_fetch/out/site_info.csv'){
+process_and_style <- function(input_data, site_filename = '1_fetch/out/site_info.csv'){
   
-  nwis_data <- read_csv(nwis_data_path)
+  nwis_data <- input_data
   site_info <- read_csv(site_filename)
   
   nwis_data_clean <- nwis_data %>%
@@ -25,10 +26,8 @@ process_and_style <- function(nwis_data_path = '2_process/out/nwis_data.csv', si
     select(-agency_cd, -X_00010_00000_cd, tz_cd) %>%
     left_join(site_info, by = "site_no") %>% 
     select(station_name = station_nm, site_no, dateTime, water_temperature, latitude = dec_lat_va, longitude = dec_long_va)
-    
-  return(nwis_data_clean)
+  
+  #return(nwis_data_clean)
+  write_csv(nwis_data_clean, '2_process/out/nwis_data_clean.csv')
   
 }
-
-
-
